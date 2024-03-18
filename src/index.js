8317f13cd3fca69d8ce53bcd04fc8b6aa2e9ff90
Faesalkhan <pathanf9545@gmail.com>
2024-03-18 09:34:54 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./bootstrap";
+import appRouter from "./App";
+import { RouterProvider } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
