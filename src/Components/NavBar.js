84ff247eab5dfc27cd3ae43a16ea0082a859fcd9
Faesalkhan <pathanf9545@gmail.com>
2024-03-23 12:46:14 +0530

    updated cart items length and links for related category

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,12 +2,14 @@ import React from "react";
 import logo from "./Assets/logo.png";
 import cartlogo from "./Assets/cart_icon.png";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 const NavBar = () => {
+  const totalCartItems = useSelector((item) => item.cart.totalQuantity);
   return (
     <div className="navbar navbar-light bg-light ">
       <div className="navbar-logo d-flex align-items-center ">
         <img src={logo} alt=""></img>
-        <span>SHOPPER</span>
+        <span>TRENDYSHOP</span>
       </div>
       <ul className="nav nav-underline ">
         <li className="nav-item">
@@ -16,30 +18,32 @@ const NavBar = () => {
           </Link>
         </li>
         <li className="nav-item">
-          <Link to="/category/men" className="nav-link mx-sm-2  mx-md-4">
+          <Link to="/men" className="nav-link mx-sm-2  mx-md-4">
             Men
           </Link>
         </li>
         <li className="nav-item">
-          <Link to="/category/women" className="nav-link mx-sm-2  mx-md-4">
+          <Link to="/women" className="nav-link mx-sm-2  mx-md-4">
             Women
           </Link>
         </li>
         <li className="nav-item">
-          <Link to="/category/kids" className="nav-link mx-sm-2  mx-md-4">
+          <Link to="/kids" className="nav-link mx-sm-2  mx-md-4">
             Kids
           </Link>
         </li>
       </ul>
       <div className="position-relative me-2 logincart">
         <Link to="/login">
-          <button className="btn btn-primary rounded-5 me-md-3">Login</button>
+          <button className="btn btn-light border-black fw-bold  rounded-5 me-md-3">
+            Login
+          </button>
         </Link>
         <Link to="/cart">
           <img src={cartlogo} alt="" className="img-fluid"></img>
         </Link>
-        <span className="bg-danger badge rounded-pill px-1 position-absolute top-0 start-100 translate-middle ">
-          99
+        <span className="bg-danger badge rounded-pill position-absolute start-100 translate-middle  ">
+          {totalCartItems}
         </span>
       </div>
     </div>
