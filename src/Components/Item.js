39e40d48c934a94b556eae8ff8785a70f92fddcd
Faesalkhan <pathanf9545@gmail.com>
2024-03-24 10:46:00 +0530

    updated prices and scroll to top

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,19 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 const Item = ({ item }) => {
   return (
     <div className="col-3">
-      <div className="d-flex flex-column item">
-        <img src={item.image} alt="" className="img-fluid"></img>
-        <p className="mb-0">{item.name}</p>
-        <div className="d-flex justify-content-start">
-          <p className="fw-bold text-nowrap old-prices me-4">
-            $ {item.old_price}
-          </p>
-          <p className="fw-light text-nowrap text-decoration-line-through  new-prices">
-            $ {item.new_price}
-          </p>
+      <Link
+        to={`/product/${item.id}`}
+        className="text-decoration-none text-black "
+        onClick={window.scrollTo(0, 0)}
+      >
+        <div className="d-flex flex-column item">
+          <img src={item.image} alt="" className="img-fluid"></img>
+          <p className="mb-0">{item.name}</p>
+          <div className="d-flex justify-content-start">
+            <p className="fw-bold text-nowrap old-prices me-4">
+              $ {item.new_price}
+            </p>
+            <p className="fw-light text-nowrap text-decoration-line-through">
+              $ {item.old_price}
+            </p>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
