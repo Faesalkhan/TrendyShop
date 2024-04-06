ec1f0ef3b30d8495c5a2ca34b8d136828ac9d9b4
Faesalkhan <pathanf9545@gmail.com>
2024-04-06 10:41:55 +0530

    added jest testing framework

---
+++ src/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  verbose: true,
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+};
